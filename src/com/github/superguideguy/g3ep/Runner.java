a4fc1e087d3cb84aeacda2d777ca1b6c9011b549
superguideguy <46546836+superguideguy@users.noreply.github.com>
2020-08-28 19:24:20 -0500

    Add SavPatcher.java

--- src/com/github/superguideguy/g3ep/Runner.java
+++ src/com/github/superguideguy/g3ep/Runner.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 public class Runner {
 
 	public static void main(String[] args) throws IOException {
-		boolean isEmerald = Step2.step2(args); // 0 = srcROM, 1 = destROM
+		Step2.step2(args); // 0 = srcROM, 1 = destROM
 		Step3.step3(args); // 2 = srcSAV, 3 = destSAV
+		SavPatcher.patchSav(args[3], args[4]);
 	}
 
 }
