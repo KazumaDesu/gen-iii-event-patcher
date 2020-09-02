b223aac22a34c1a6f16331f6cdf826014fa3381c
superguideguy <46546836+superguideguy@users.noreply.github.com>
2020-09-02 10:52:18 -0500

    Update BinCreator.java

--- src/com/github/superguideguy/g3ep/BinCreator.java
+++ src/com/github/superguideguy/g3ep/BinCreator.java
@@ -3,16 +3,34 @@ package com.github.superguideguy.g3ep;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class BinCreator implements Runnable, ActionListener {
 
-	JFrame		frame;
-	JTabbedPane	pane;
-	JPanel		panelNews, panelCard, panelEvent;
+	JFrame			frame;
+	JTabbedPane		pane;
+	JPanel			panelNews, panelCard, panelEvent;
+
+	JSpinner		newsId;
+	ButtonGroup		newsDistributeGroup, newsColorGroup;
+	JRadioButton	newsNonDistribute, newsReDistribute;
+	JRadioButton	newsColorYellow, newsColorCyan, newsColorRed, newsColorGreen, newsColorBlue, newsColorOlive,
+			newsColorGold, newsColorSilver;
+	JTextField		newsText[];
+
+	JSpinner		cardId;
+	JSpinner		icon;
+	// TODO reserved elements and flags/color
+	JTextField		cardText[];
+
+	// TODO panelEvent
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
