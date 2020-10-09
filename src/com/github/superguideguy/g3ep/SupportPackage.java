995049ba0f5a558b9a13da4e21f037b710e5e75a
superguideguy <46546836+superguideguy@users.noreply.github.com>
2020-10-09 17:27:30 -0500

    Update SupportPackage.java

--- src/com/github/superguideguy/g3ep/SupportPackage.java
+++ src/com/github/superguideguy/g3ep/SupportPackage.java
@@ -4,10 +4,6 @@ package com.github.superguideguy.g3ep;
  * TODO Get locations for JPN_RS
  * TODO Get locations for JPN_FRLG
  * TODO Get locations for JPN_EM
- * TODO Confirm all INTL SAV files have identical structure
- * TODO Confirm 1st bit location for INTL_RS
- * TODO Confirm 2nd bit for INTL_RS is irrelevant
- * TODO Confirm 2nd bit for INTL_EM
  */
 public enum SupportPackage {
 
@@ -20,11 +16,11 @@ public enum SupportPackage {
 	//JPN_EM		(new int[][] {/* ??? */},
 			//new int[] {0x0000,______,______,	0x43aC,0x0000,0x4490,0x0000,	______,______}),
 
-	INTL_RS		(new int[][] {/*{0x23a9,4}*/},
+	INTL_RS		(new int[][] {{0x23a9,4}},
 			new int[] {0x42e0,0x0000,0x0498,	0x0000,0x0000,0x0000,0x0000,	0x4810,0x4bfc}),
 	INTL_FRLG	(new int[][] {{0x2067,1}},
 			new int[] {0x0000,0x426c,0x04a0,	0x0000,0x42a0,0x0000,0x4460,	0x479c,0x0000}),
-	INTL_EM		(new int[][] {{0x240b,3},/*{0x2405,4}*/},
+	INTL_EM		(new int[][] {{0x240b,3},{0x2405,4}},
 			new int[] {0x0000,0x4378,0x0bec,	0x0000,0x43ac,0x0000,0x456c,	0x48a8,0x4c94}),
 	;
 	// @formatter:on
